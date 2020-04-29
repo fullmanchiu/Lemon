360ed0e0cbf2e36dc35d877d0b2f05ecf82ebdb4
lance <fullmanchiu@gmail.com>
2020-04-29 09:56:39 +0800

    Initial commit

---
+++ app/src/main/java/com/fullmanchiu/lemon/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.fullmanchiu.lemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    private static final String TAG = BaseActivity.class.getSimpleName();
+}
